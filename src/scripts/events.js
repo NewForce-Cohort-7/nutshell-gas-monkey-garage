e1f045cbc2d23405a1e50f31aad40cf5473cee67
Spencer Lott <spklott@gmail.com>
2023-04-04 11:44:43 -0400

    aarons changes

--- src/scripts/events.js
+++ src/scripts/events.js
@@ -16,17 +16,21 @@ mainContainer.addEventListener("click", click => {
 })
 
 const convertEventToListElement = (eventObject) => {
-        return `<li class="eventComplete">
-        ${eventObject.name} 
-        <button class="event__delete" id="event--${eventObject.id}">-
-        </button>
-   
-</li>`
+    return `
+    <li class="eventComplete">
+    ${eventObject.name} 
+    <button class="event__delete" id="event--${eventObject.id}">-
+    </button>
+    
+    </li>`
 }
 
 export const Events = () => {
     const events = getEvents()
     const sortEvents = events.sort((a,b) => new Date(a.date) - new Date(b.date))
+
+    // they should see all of their events sorted into months, with a heading for each month and the total number of events in that month in parentheses beside the heading
+    //sort the sortEvents by month. To do so I will 
 
     let html = `
         <ul class="eventsList"> 
@@ -40,54 +44,11 @@ export const Events = () => {
 }
 
 export const OpenForm = () => {
-  return`<button id="open-form">Open Form</button>`
+  return`<button id="open-form">Create New</button>`
 }
 
 mainContainer.addEventListener("click", click => {
   if (click.target.id === "open-form") {
     mainContainer.innerHTML +=    EventForm()
- 
-    console.log("you clicked me")
-  }
+}
 })
-
-
-// need to refresh once save event is clicked.
-
-
-
-
-
-// `
-
-// <div id="form-container" style="display: none;">
-//   <label for="name">Name:</label>
-//   <input type="text" id="name" name="name">
-
-//   <label for="email">Email:</label>
-//   <input type="email" id="email" name="email">
-
-//   <button id="submit-form">Submit</button>
-// </div>`
-
-// const openFormButton = document.getElementById('open-form');
-// const formContainer = document.getElementById('form-container');
-// const submitButton = document.getElementById('submit-form');
-
-// openFormButton.addEventListener("click", () => {
-//   formContainer.style.display = "block";
-// });
-
-// submitButton.addEventListener('click', () => {
-//   const name = document.getElementById('name').value;
-//   const email = document.getElementById('email').value;
-
-//   const formData = {
-//     name,
-//     email,
-//   };
-
-//   localStorage.setItem('form-data', JSON.stringify(formData));
-
-//   formContainer.style.display = 'none';
-// });
